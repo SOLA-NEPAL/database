4e8ca85a28554bb082b880016c0ec02464606341
Alexander Solovov <solovov@gmail.com>
2012-06-17 17:04:42 +0600

    Unifying DB scripts

--- update_min_max_Extents.sql
+++ update_min_max_Extents.sql
@@ -2,7 +2,7 @@
 CREATE SCHEMA test_etl1;
 
 --parcel data table import.
-CREATE OR REPLACE FUNCTION sola.test_etl1.set_extents() RETURNS varchar
+CREATE OR REPLACE FUNCTION test_etl1.set_extents() RETURNS varchar
  AS
 $BODY$
 DECLARE 
@@ -10,13 +10,13 @@ DECLARE
 BEGIN
 	FOR rec IN EXECUTE 'SELECT min(xmin(the_geom)) as x_min,max(xmax(the_geom)) as x_max,
 		 min(ymin(the_geom)) as y_min,max(ymax(the_geom)) as y_max
-		 FROM sola.testdata.mulpani_parcel WHERE (ST_GeometryN(the_geom, 1) IS NOT NULL)'
+		 FROM testdata.mulpani_parcel WHERE (ST_GeometryN(the_geom, 1) IS NOT NULL)'
 	LOOP
-		update sola.system.setting set vl=rec.x_min where "name"='map-west';
-		update sola.system.setting set vl=rec.x_max where "name"='map-east';
-		update sola.system.setting set vl=rec.y_min where "name"='map-south';
-		update sola.system.setting set vl=rec.y_max where "name"='map-north';
-		update sola.system.setting set vl=97261 where "name"='map-srid';
+		update system.setting set vl=rec.x_min where "name"='map-west';
+		update system.setting set vl=rec.x_max where "name"='map-east';
+		update system.setting set vl=rec.y_min where "name"='map-south';
+		update system.setting set vl=rec.y_max where "name"='map-north';
+		update system.setting set vl=97261 where "name"='map-srid';
 	END LOOP;
 	
     RETURN 'ok';
@@ -26,6 +26,6 @@ $BODY$
   LANGUAGE plpgsql;
   
 --execute update procedure.
-  SELECT sola.test_etl1.set_extents();
+  SELECT test_etl1.set_extents();
   
  DROP SCHEMA IF EXISTS test_etl1 CASCADE;
